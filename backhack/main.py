e10abfa8ec1a88b3b5739e61d45f2dca4d7d48bb
TD <IsaevSlava.2001@yandex.ru>
2024-04-12 19:58:48 +0300

    third update for back

--- backhack/main.py
+++ backhack/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, File, UploadFile
 import datetime
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_percentage_error
+import joblib
 
 app = FastAPI()
 
@@ -38,4 +43,14 @@ def upload(file: UploadFile = File(...)):
 
 @app.get("/main")
 def getInfo(date: datetime.date = '27.03.2023'):
-    return {"message":"test_data_worked"}
+    df=pd.read_csv('data/df.csv')
+    rf_model = joblib.load("model/rf_100.joblib")
+    non_numerical_columns = df.select_dtypes(exclude=['number']).columns.tolist()
+
+    non_numeric_features = []
+
+    for col in non_numerical_columns:
+        non_numeric_features.append(col)
+
+    output = rf_model.predict(df.drop(non_numeric_features, axis=1).drop('Продажи, рубли', axis=1))
+    return {"message":output}
