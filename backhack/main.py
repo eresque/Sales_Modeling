c34db6303d4b1679be538e8ec6b35d7ea0047412
TD <IsaevSlava.2001@yandex.ru>
2024-04-13 01:40:13 +0300

    seventh update to back(modified get 28 date)

--- backhack/main.py
+++ backhack/main.py
@@ -44,12 +44,15 @@ def upload(file: UploadFile = File(...)):
 @app.get("/main")
 def getInfo(date: datetime.date = '2023-07-02'):
     df=pd.read_csv("./data/df.csv")
-    nearest_previous_date=[]
-    for data in df['Начало нед']:
-        if data<=date:
-            nearest_previous_date.append(data)
-    row_index = df[df['Начало нед'] == nearest_previous_date[-1]].index[0]
-    next_28_rows = df.iloc[row_index:row_index+28]
+    df['Начало нед'] = pd.to_datetime(df['Начало нед'])
+    user_inp=pd.to_datetime(date)
+    print(type(df['Начало нед']))
+    print(type(user_inp))
+    filtered_df = df[df['Начало нед'] <= user_inp]
+    sorted_df = filtered_df.sort_values(by='Начало нед', ascending=False)
+    closest_date = sorted_df.iloc[0]['Начало нед']
+    index = df[df['Начало нед'] == closest_date].index[0]
+    next_28_rows = df.iloc[index:index+28]
     rf_model = joblib.load("./model/rf_100.joblib")
     non_numerical_columns = next_28_rows.select_dtypes(exclude=['number']).columns.tolist()
 
--- backhack/test.py
+++ backhack/test.py
@@ -3,13 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 
 def getInfo(date: datetime.date = '2023-07-02'):
-    df=pd.read_csv("backhack/data/df.csv")
-    nearest_previous_date=[]
-    for data in df['Начало нед']:
-        if data<=date:
-            nearest_previous_date.append(data)
-    row_index = df[df['Начало нед'] == nearest_previous_date[-1]].index[0]
-    next_28_rows = df.iloc[row_index:row_index+28]
+    df=pd.read_csv("data/df.csv")
+    df['Начало нед'] = pd.to_datetime(df['Начало нед'])
+    user_inp=pd.to_datetime(date)
+    print(type(df['Начало нед']))
+    print(type(user_inp))
+    filtered_df = df[df['Начало нед'] <= user_inp]
+    sorted_df = filtered_df.sort_values(by='Начало нед', ascending=False)
+    closest_date = sorted_df.iloc[0]['Начало нед']
+    index = df[df['Начало нед'] == closest_date].index[0]
+    next_28_rows = df.iloc[index:index+28]
     print(next_28_rows)
 
 
